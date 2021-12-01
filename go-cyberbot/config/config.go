4fcdf2a33b1b202b4dd50d87b115cdd01bf52851
FKouhai <frandres00@gmail.com>
2021-12-01 22:27:51 +0100

    Upgraded the config

--- go-cyberbot/config/config.go
+++ go-cyberbot/config/config.go
@@ -18,13 +18,11 @@ type configStruct struct { //config struct which contains the bot Token and its 
 }
 
 func ReadConfig() error { //Function that reads the config file using json unmarshal
-  fmt.Println("Reading config file")
   file, err := ioutil.ReadFile("./config.json")
   if err != nil {
     fmt.Println(err.Error())
     return err
   }
-  fmt.Println(string(file))
   err = json.Unmarshal(file, &config)
   if err != nil {
     fmt.Println(err.Error())
